a1fcad020b74158697bd5d7767347a22f1e15bad
Fabian Schilling <fabian.schilling@me.com>
2017-01-10 12:06:35 +0100

    Make SSK fast with LRU cache decorator

--- code/ssk.py
+++ code/ssk.py
@@ -1,25 +1,47 @@
+__author__ = 'Fabian Schilling'
+__email__ = 'fabsch@kth.se'
+
 import numpy as np
+import functools
 
 def ssk(doc1, doc2, n=2, lam=0.5, norm=True):
-    a = list(doc1)
-    b = list(doc2)
-    Kp = np.zeros((n + 1, len(a), len(b)), dtype=np.float64)
-    Kp[0, :] = 1.0
-    for i in range(n):
-        for j in range(len(a) - 1):
-            Kpp = 0.0
-            for k in range(len(b) - 1):
-                Kpp = lam * (Kpp + lam * int(a[j] == b[k]) * Kp[i, j, k])
-                Kp[i+1, j+1, k+1] = lam * Kp[i+1, j, k+1] + Kpp
-    K = 0.0
-    for i in range(n):
-        for j in range(len(a)):
-            for k in range(len(b)):
-                K += lam * lam * int(a[j] == b[k]) * Kp[i, j, k]
-    
-    if norm:
-        K1 = ssk(doc1, doc1, n=n, lam=lam, norm=False)
-        K2 = ssk(doc2, doc2, n=n, lam=lam, norm=False)
-        return K / np.sqrt(K1 * K2)
+  """Computes the string subsequence kernel (SSK)
+  Args:
+    doc1: First document
+    doc2: Second document
+    n: Maximum length of the subsequence
+    lam: Decay factor
+    norm: Normalize to equal document lenghts
 
-    return K
+  Returns:
+    Similarity between the two documents
+  """
+  result = K(doc1, doc2, n, lam)
+  if norm:
+    return result / np.sqrt(K(doc1, doc1, n, lam) * K(doc2, doc2, n, lam))
+  return result
+
+
+@functools.lru_cache(maxsize=None)
+def K(s, t, n, lam):
+  if min(len(s), len(t)) < n:
+    return 0.0
+  jsum = 0.0
+  for j in range(len(t)):
+    if t[j] == s[-1]:
+      jsum += Kp(s[:-1], t[:j], n-1, lam)
+  return K(s[:-1], t, n, lam) + lam ** 2 * jsum
+
+
+@functools.lru_cache(maxsize=None)
+def Kp(s, t, n, lam):
+  if n == 0:
+    return 1.0
+  if min(len(s), len(t)) < n:
+    return 0.0
+  jsum = 0.0
+  for j in range(len(t)):
+    if t[j] == s[-1]:
+      jsum += Kp(s[:-1], t[:j], n-1, lam) * (lam ** (len(t) - (j + 1) + 2))
+  return lam * Kp(s[:-1], t, n, lam) + jsum
+
